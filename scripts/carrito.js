40a2de31d5bdbe33fab7a40b9618078010ec22e2
max-sosa <90153004+max-sosa@users.noreply.github.com>
2021-12-06 16:19:13 -0300

    Mejorando el carrito

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -1,7 +1,7 @@
 let carrito = [];
 
 let montoTotalCompra = document.getElementById("montoTotal");
-montoTotalCompra.innerText = "0";
+montoTotalCompra.innerText = "$ 0";
 
 let cantProductos = document.getElementById("cantidadTotal");
 cantProductos.innerText = "0";
@@ -9,34 +9,30 @@ cantProductos.innerText = "0";
 let botonFinalizar = document.getElementById("btnFinalizar");
 botonFinalizar.onclick = () => {
     const precioFinal = montoTotalCompra.innerText;
-    alert("EL MONTO DE LA COMPRA TOTAL ES $" + precioFinal);
+    alert("EL MONTO TOTAL DE LA COMPRA ES " + precioFinal);
 }
 
-function agregarCarrito(){
-    let padre = document.getElementById("tableBody");
-    let contenedor = document.createElement("tr");
-    contenedor.innerHTML = `<th scope="row"> ${productos[4].id}</th>
-                            <td> ${productos[4].name}</td>
-                            <td> ${0}</td>
-                            <td> $ ${productos[4].price}</td>`;
-    padre.appendChild(contenedor);
-    carrito.push(productos[4]);
-    console.log(carrito);
-    localStorage.setItem("carritoRustico", JSON.stringify(carrito));
+
+for(const prod of productos){
+   document.getElementById(`${prod.id}`).onclick = () => agregarAlCarro(`${prod.id}`);
+}
+
+function agregarAlCarro(miId) {
+    $("#tableBody").append(
+        `<tr><th scope="row"> ${productos[miId - 1].id}</th>
+        <td> ${productos[miId - 1].name}</td>
+        <td> ${1}</td>
+        <td> $ ${productos[miId - 1].price}</td></tr>`);
+    carrito.push(productos[miId - 1]);
+    localStorage.setItem("Carrito", JSON.stringify(carrito));
     calcularTotalCarrito();
 };
-
-let botonComprar = document.getElementById("5");
-botonComprar.addEventListener("click",agregarCarrito);
-
-
 
 function calcularTotalCarrito() {
     let total = 0;
     for (const prodEnCarro of carrito) {
         total += prodEnCarro.price;
-        console.log(total);
     }
-    montoTotalCompra.innerText = total;
+    montoTotalCompra.innerText = "$ "+ total;
     cantProductos.innerText = carrito.length;
 }
--- scripts/tienda.js
+++ scripts/tienda.js
@@ -1,9 +1,10 @@
 //Creación de la clase > Product
 class product{
-    constructor (id,name,type,price){
+    constructor (id,name,type,photo,price){
         this.id = id;
         this.name = name;
         this.type = type;
+        this.photo = photo;
         this.price = parseFloat(price);
     }
 
@@ -13,26 +14,26 @@ class product{
 }
 
 //Creación de objetos > Panificados
-const ciabatta = new product(1,"Ciabatta","Panificado",100);
-const figaza = new product(2,"Figaza", "Panificado",150);
-const negritos = new product(3,"Negritos Integrales", "Panificado",200);
-const burger = new product(4,"Pan Burger", "Panificado",250);
-const brioche = new product(5,"Brioche", "Panificado",300);
-const integral = new product(6,"Pan Integral","Panificado",350);
-const scons = new product(7,"Scons", "Panificado",400);
-const medialunas = new product(8,"Medialunas", "Panificado",450);
-const panChocolate = new product(9,"Pan de Chocolate", "Panificado",500);
+const ciabatta = new product(1,"Ciabatta","Panificado","images/productos/panificados/ciabatta.jpg",100);
+const figaza = new product(2,"Figaza", "Panificado","images/productos/panificados/figazita.jpg",150);
+const negritos = new product(3,"Negritos Integrales", "Panificado","images/productos/panificados/negritos integrales.jpg",200);
+const burger = new product(4,"Pan Burger", "Panificado","images/productos/panificados/pan hamburguesas.jpg",250);
+const brioche = new product(5,"Brioche", "Panificado","images/productos/panificados/brioche.jpg",300);
+const integral = new product(6,"Pan Integral","Panificado","images/productos/panificados/pan integral.jpg",350);
+const scons = new product(7,"Scons", "Panificado","images/productos/panificados/scons.jpg",400);
+const medialunas = new product(8,"Medialunas", "Panificado","images/productos/panificados/medialunas.jpg",450);
+const panChocolate = new product(9,"Pan de Chocolate", "Panificado","images/productos/panificados/pan de chocolate.jpg",500);
 
 //Creando lista de panificados.
 const panificados = [ciabatta, figaza, negritos, burger, brioche, integral, scons, medialunas, panChocolate];
 
 //Creación de objetos > Tortas
-const appleCrumble = new product(10,"Apple Crumble", "Tortas",1000);
-const cheesecake = new product(11,"Cheesecake", "Tortas",1500);
-const tortaMarisa = new product(12,"Torta Marisa", "Tortas",2000);
-const lemonPie = new product(13,"Lemon Pie", "Tortas",2500);
-const carrotCake = new product(14,"Carrot Cake", "Tortas",3000);
-const dobleChocolate = new product(15,"Doble Chocolate", "Tortas",3500);
+const appleCrumble = new product(10,"Apple Crumble", "Tortas","images/productos/tortas/apple crumble.jpg",1000);
+const cheesecake = new product(11,"Cheesecake", "Tortas","images/productos/tortas/cheesecake.jpg",1500);
+const tortaMarisa = new product(12,"Torta Marisa", "Tortas","images/productos/tortas/torta marisa.jpg",2000);
+const lemonPie = new product(13,"Lemon Pie", "Tortas","images/productos/tortas/lemon pie.jpg",2500);
+const carrotCake = new product(14,"Carrot Cake", "Tortas","images/productos/tortas/carrot cake.jpg",3000);
+const dobleChocolate = new product(15,"Doble Chocolate", "Tortas","images/productos/tortas/doble chocolate.jpg",3500);
 
 //Creando lista de tortas.
 const tortas = [appleCrumble, cheesecake, tortaMarisa, lemonPie, carrotCake, dobleChocolate];
@@ -40,3 +41,43 @@ const tortas = [appleCrumble, cheesecake, tortaMarisa, lemonPie, carrotCake, dob
 //Concatenando array de tortas y panificiados en un solo array de productos.
 
 const productos = panificados.concat(tortas);
+
+//Renderizado de la tienda
+
+for(const pan of panificados){
+    $(".breadCard").append(
+        `<div class="card mb-3" style="max-width: 56rem;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="${pan.photo}" class="img-fluid rounded-start" alt="${pan.name}">
+            </div>
+            <div class="col-md-8 my-auto">
+                <div class="card-body text-center">
+                    <h5 class="card-title font-monospace">${pan.name}</h5>
+                    <p class="card-text font-monospace">$${pan.price}</p>
+                    <p class="card-text font-monospace">(Precio por unidad)</p>
+                    <button id="${pan.id}" type="button" class="btn btn-outline-dark">Agregar al carrito</button>
+                </div>
+            </div>
+        </div>`);
+}
+
+for(const torta of tortas){
+    $(".cakeCard").append(
+        `<div class="card mb-3" style="max-width: 56rem;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="${torta.photo}" class="img-fluid rounded-start" alt="${torta.name}">
+            </div>
+            <div class="col-md-8 my-auto">
+                <div class="card-body text-center">
+                    <h5 class="card-title font-monospace">${torta.name}</h5>
+                    <p class="card-text font-monospace">$${torta.price}</p>
+                    <p class="card-text font-monospace">(Precio por unidad)</p>
+                    <button id="${torta.id}" type="button" class="btn btn-outline-dark">Comprar</button>
+                </div>
+            </div>
+        </div>`);
+}
+
+
